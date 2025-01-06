df9d09e135ba7a048ba869f1b201f6ee20ee6532
z36o6 <alshhrymsb95@gmail.com>
2025-01-06 22:24:58 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 
+
 module.exports = {
     token: '',
     clientId: '1273230302822797312',
     guildId: '1106591466651922502',
     youtubeApiKey: process.env.AIzaSyDekrJzUSrL1QLmhjsd9MyLgh6ofvf8UXU,
-   mongoUri: process.env.mongodb+srv.//z36o6:Aa_123456@z36o6.ha7kd.mongodb.net/?retryWrites=true&w=majority&appName=z36o6,
-   
-       cookies:{
-    
+    mongoUri: process.env.z36o6:Aa_123456@z36o6.ha7kd.mongodb.net/?retryWrites=true&w=majority&appName=z36o6,
+    spotify: {
+        clientId: process.env.dcdf5fe8bd084f59acc72e939b8abc75,
+        clientSecret: process.env.89480622a0604925b3d2813c864b5c12,
+    },
+    cookies:{
         domain: ".youtube.com",
         expirationDate: 1757764090.235338,
         hostOnly: false,
@@ -22,12 +25,13 @@ module.exports = {
         value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
         id: 1
     },
+    
     distubeOptions: {
         emitAddListWhenCreatingQueue: true,
         emitAddSongWhenCreatingQueue: false,
         emitNewSongOnly: true,
         joinNewVoiceChannel: true,
         nsfw: true,
-        savePreviousSongs: true
+        savePreviousSongs: true,
     }
 };
