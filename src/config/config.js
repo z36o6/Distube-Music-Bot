bf81f93f866f6fc852a6a369b71da504e26550ca
z36o6 <alshhrymsb95@gmail.com>
2025-01-06 04:12:28 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     guildId: '1106591466651922502',
     youtubeApiKey: process.env.AIzaSyDekrJzUSrL1QLmhjsd9MyLgh6ofvf8UXU,
    mongoUri: process.env.mongodb+srv.//z36o6:Aa_123456@z36o6.ha7kd.mongodb.net/?retryWrites=true&w=majority&appName=z36o6,
-    cookies: {
+    cookies :{
         domain: ".youtube.com",
         expirationDate: 1757764090.235338,
         hostOnly: false,
