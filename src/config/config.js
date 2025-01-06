306b6f373f0e59baf660135e061277e8814a557d
z36o6 <alshhrymsb95@gmail.com>
2025-01-06 03:50:57 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
 
-
 module.exports = {
     token: '',
     clientId: '1273230302822797312',
     guildId: '1106591466651922502',
     youtubeApiKey: process.env.AIzaSyDekrJzUSrL1QLmhjsd9MyLgh6ofvf8UXU,
-    mongoUri: process.env.mongodb+srv.//z36o6:Aa_123456@z36o6.ha7kd.mongodb.net/?retryWrites=true&w=majority&appName=z36o6
-   
-    },
-    cookies:{
+    mongoUri: process.env.mongodb+srv://z36o6:Aa_123456@z36o6.ha7kd.mongodb.net/?retryWrites=true&w=majority&appName=z36o6,
+    cookies: {
         domain: ".youtube.com",
         expirationDate: 1757764090.235338,
         hostOnly: false,
@@ -23,13 +20,12 @@ module.exports = {
         value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
         id: 1
     },
-    
     distubeOptions: {
         emitAddListWhenCreatingQueue: true,
         emitAddSongWhenCreatingQueue: false,
         emitNewSongOnly: true,
         joinNewVoiceChannel: true,
         nsfw: true,
-        savePreviousSongs: true,
+        savePreviousSongs: true
     }
 };
